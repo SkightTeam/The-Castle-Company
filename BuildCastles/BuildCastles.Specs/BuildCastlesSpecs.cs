dd6467354f59aaf7aa7adbf3232c6713efc14e54
王昊 <wanghao@skight.com>
2017-05-17 11:39:52 -0700

    Add more cases for trends

--- BuildCastles/BuildCastles.Specs/BuildCastlesSpecs.cs
+++ BuildCastles/BuildCastles.Specs/BuildCastlesSpecs.cs
@@ -20,7 +20,21 @@ namespace BuildCastles
 
     public class TransformTrendSpecs
     {
-        It should= ()
-            =>new[] {1, 2, 3, 4}.to_trends().SequenceEqual(new[] {Trend.Down, Trend.Up, Trend.Up}).ShouldBeTrue();
+        It all_increase_height_lead_up_trends= ()
+            =>new[] {1, 2, 3, 4}.to_trends().SequenceEqual(new[] {Trend.Up, Trend.Up, Trend.Up}).ShouldBeTrue();
+
+        It all_decrease_height_lead_down_trends = ()
+             => new[] { 4,3,2,1 }.to_trends().SequenceEqual(new[] { Trend.Down, Trend.Down, Trend.Down }).ShouldBeTrue();
+
+        It increase_and_decrease_case = ()
+            => new[] { 1,2,3,2,1 }.to_trends().SequenceEqual(
+                new[] { Trend.Up, Trend.Up, Trend.Down,Trend.Down }).ShouldBeTrue();
+
+        It decrease_and_increase_case = ()
+           => new[] { 3,2,1,2,3 }.to_trends().SequenceEqual(
+               new[] { Trend.Down, Trend.Down, Trend.Up, Trend.Up }).ShouldBeTrue();
+
     }
+
+   
 }
---
+++ BuildCastles/BuildCastles.Specs/TrendSpecs.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Machine.Specifications;
+
+namespace BuildCastles
+{
+    public class TrendSpecs
+    {
+        It all_increase_height_lead_up_trends= ()
+            =>new[] {1, 2, 3, 4}.to_trends().SequenceEqual(new[] {Trend.Up, Trend.Up, Trend.Up}).ShouldBeTrue();
+
+        It all_decrease_height_lead_down_trends = ()
+            => new[] { 4,3,2,1 }.to_trends().SequenceEqual(new[] { Trend.Down, Trend.Down, Trend.Down }).ShouldBeTrue();
+
+        It increase_and_decrease_case = ()
+            => new[] { 1,2,3,2,1 }.to_trends().SequenceEqual(
+                new[] { Trend.Up, Trend.Up, Trend.Down,Trend.Down }).ShouldBeTrue();
+
+        It decrease_and_increase_case = ()
+            => new[] { 3,2,1,2,3 }.to_trends().SequenceEqual(
+                new[] { Trend.Down, Trend.Down, Trend.Up, Trend.Up }).ShouldBeTrue();
+
+    }
+}
