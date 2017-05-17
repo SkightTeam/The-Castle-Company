52db5133a9420df88cd61a186494b37d47998de0
王昊 <wanghao@skight.com>
2017-05-17 11:03:24 -0700

    Add first test to express my understanding about requirment

---
+++ BuildCastles/BuildCastles.Specs/BuildCastlesSpecs.cs
@@ -0,0 +1,19 @@
+﻿using BuildCastles;
+using Machine.Specifications;
+
+namespace BuildCastles
+{
+    public class BuildCastlesSpecs
+    {
+        private Because of = () => result = subject.count_castles();
+        protected static int[] subject;
+        protected static int result;
+    }
+    public class When_only_1_peak: BuildCastlesSpecs
+    {
+        Establish context = () => subject = new int[] {1, 2, 3, 2, 1};
+
+        It should_count_1 = () => result.ShouldEqual(1);
+
+    }
+}
---
+++ BuildCastles/BuildCastles/CastleBuildingAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BuildCastles
+{
+    public static class CastleBuildingAlgorithm
+    {
+        public static int count_castles(this IEnumerable<int> source)
+        {
+            var result = 0;
+           
+            return result;
+        }
+    }
+}
